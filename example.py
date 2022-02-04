665ad1c571fb076d2e1256443fb8ed6a0a8cac28
tjklein <TJKlein@gmail.com>
2022-02-04 02:28:07 +0000

    added SentEval

--- example.py
+++ example.py
@@ -5,6 +5,27 @@ import os
 import wandb
 import datasets
 
+import transformers
+from transformers import (
+    CONFIG_MAPPING,
+    MODEL_FOR_MASKED_LM_MAPPING,
+    AutoConfig,
+    AutoModelForMaskedLM,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    DataCollatorWithPadding,
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+    default_data_collator,
+    set_seed,
+    EvalPrediction,
+    BertModel,
+    BertForPreTraining,
+    RobertaModel
+)
+
 parser.add_argument('--model_name_or_path', required=True, type=str, help='Mode for filename to Huggingface model')
 parser.add_argument('--train_file', type=str, help='Input training file (text)')
 parser.add_argument('--output_dir', required=True, type=str, help='Path where result should be stored')
@@ -121,6 +142,55 @@ if __name__ == '__main__':
             remove_columns=column_names,
             load_from_cache_file=not FLAGS.overwrite_cache,
         )
+    
+    
+     # Data collator
+    @dataclass
+    class OurDataCollatorWithPadding:
+
+        tokenizer: PreTrainedTokenizerBase
+        padding: Union[bool, str, PaddingStrategy] = True
+        max_length: Optional[int] = None
+        pad_to_multiple_of: Optional[int] = None
+        mlm: bool = True
+        mlm_probability: float = data_args.mlm_probability
+
+        def __call__(self, features: List[Dict[str, Union[List[int], List[List[int]], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+            special_keys = ['input_ids', 'attention_mask', 'token_type_ids', 'mlm_input_ids', 'mlm_labels']
+            bs = len(features)
+            if bs > 0:
+                num_sent = len(features[0]['input_ids'])
+            else:
+                return
+            flat_features = []
+            for feature in features:
+                for i in range(num_sent):
+                    flat_features.append({k: feature[k][i] if k in special_keys else feature[k] for k in feature})
+
+            batch = self.tokenizer.pad(
+                flat_features,
+                padding=self.padding,
+                max_length=self.max_length,
+                pad_to_multiple_of=self.pad_to_multiple_of,
+                return_tensors="pt",
+            )
+            if model_args.do_mlm:
+                batch["mlm_input_ids"], batch["mlm_labels"] = self.mask_tokens(batch["input_ids"])
+
+            batch = {k: batch[k].view(bs, num_sent, -1) if k in special_keys else batch[k].view(bs, num_sent, -1)[:, 0] for k in batch}
+
+            if "label" in batch:
+                batch["labels"] = batch["label"]
+                del batch["label"]
+            if "label_ids" in batch:
+                batch["labels"] = batch["label_ids"]
+                del batch["label_ids"]
+
+            return batch
+        
+
+    data_collator = default_data_collator if FLAGS.pad_to_max_length else OurDataCollatorWithPadding(tokenizer)
+    
     
     
     
