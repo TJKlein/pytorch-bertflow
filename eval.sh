4a47cef47749cbfd0b72acf05bc5afe834283cdc
Tassilo Klein <TJKlein@gmail.com>
2022-02-05 00:58:00 +0100

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 python evaluation.py \
-    --model_name_or_path result/crimson-shadow-1 \
     --pooler cls \
     --task_set sts \
-    --mode test
+    --mode test \
+    --model_name_or_path "$@"
